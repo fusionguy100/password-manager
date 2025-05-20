77dc117df279491db6f36bdfc5b31243a816be1e
Jacob Newell <163172507+fusionguy100@users.noreply.github.com>
2025-05-20 15:49:28 -0700

    import logo and window

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import tkinter
+from tkinter import *
+
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 # ---------------------------- UI SETUP ------------------------------- #
+
+window = Tk()
+window.title("Password Manager")
+window.config(padx= 20, pady= 20)
+
+logo_img = PhotoImage(file="logo.png")
+canvas = Canvas(height = 200, width = 200)
+canvas.create_image(100,100,image=logo_img)
+canvas.pack()
+
+
+
+
+
+tkinter.mainloop()
